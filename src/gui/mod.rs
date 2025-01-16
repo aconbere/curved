2a800ae06f823d7a423c1e6b70aa0488b56e561e
Anders Conbere <anders@conbere.org>
2025-01-15 17:27:32 -0800

    more gui!

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -16,7 +16,6 @@ use super::generate;
 struct ImageState {
     path: PathBuf,
     image: DynamicImage,
-    preview: TextureBufferedImage,
 }
 
 enum AnalyzeStates {
@@ -25,12 +24,17 @@ enum AnalyzeStates {
     Analyzed(analyze::AnalyzeResults),
 }
 
-struct ApplyState {
+enum ApplyStates {
+    WaitingForImageAndCurve(WaitingForImageAndCurveState),
+    Applied(AppliedState),
+}
+
+struct WaitingForImageAndCurveState {
     curve: Option<Spline<f64, f64>>,
     image: Option<DynamicImage>,
-    output_file: Option<PathBuf>,
-    preview: Option<TextureBufferedImage>,
 }
+
+struct AppliedState {}
 
 #[derive(Default)]
 enum AppStates {
@@ -38,7 +42,7 @@ enum AppStates {
     GenerateOrAnalyze,
     Analyze(AnalyzeStates),
     Generate,
-    ApplyCurve(ApplyState),
+    Apply(ApplyStates),
 }
 
 #[derive(Default)]
@@ -46,6 +50,7 @@ struct CurvedApp {
     state: AppStates,
     debug: bool,
     image_state: Option<ImageState>,
+    preview: Option<TextureBufferedImage>,
     process: String,
     notes: String,
 }
@@ -138,142 +143,152 @@ pub fn start(debug: bool) {
 
 impl eframe::App for CurvedApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| match &mut self.state {
-            AppStates::GenerateOrAnalyze => {
-                if ui.button("Generate Step Wedge").clicked() {
-                    self.state = AppStates::Generate;
-                }
-                if ui.button("Analyze Scan").clicked() {
-                    self.state = AppStates::Analyze(AnalyzeStates::WaitingForImage);
-                }
-                if ui.button("Apply Curve").clicked() {
-                    self.state = AppStates::ApplyCurve(ApplyState {
-                        curve: None,
-                        image: None,
-                        output_file: None,
-                        preview: None,
-                    });
-                }
-            }
-            AppStates::Generate => {
-                // Consider writing these to a label in the image
-                // Maybe store a list of procesess to choose from
-                // and add new ones?
-                ui.vertical(|ui| {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            match self.state {
+                AppStates::GenerateOrAnalyze => {}
+                _ => {
                     if ui.button("Back").clicked() {
                         self.state = AppStates::GenerateOrAnalyze
                     }
-                    ui.horizontal(|ui| {
-                        let process_label = ui.label("Process: ");
-                        ui.text_edit_singleline(&mut self.process)
-                            .labelled_by(process_label.id);
-                    });
-                    ui.horizontal(|ui| {
-                        let notes_label = ui.label("Notes: ");
-                        ui.text_edit_singleline(&mut self.notes)
-                            .labelled_by(notes_label.id);
-                    });
-                });
-
-                if ui.button("Generate").clicked() {
-                    if let Some(path) = rfd::FileDialog::new().save_file() {
-                        let image = generate::generate(self.debug);
-                        image.save(path).unwrap();
-                    }
                 }
             }
-            AppStates::ApplyCurve(ref mut apply_state) => {
-                if ui.button("Select Image").clicked() {
-                    if let Some(image_file) = rfd::FileDialog::new().pick_file() {
-                        let image = image::open(&image_file).unwrap();
-                        let preview = TextureBufferedImage::new(
-                            image_file.clone().into_os_string().into_string().unwrap(),
-                            &image,
-                        );
-                        apply_state.image = Some(image);
-                        apply_state.preview = Some(preview)
+            match &mut self.state {
+                AppStates::GenerateOrAnalyze => {
+                    if ui.button("Generate Step Wedge").clicked() {
+                        self.state = AppStates::Generate;
                     }
-                };
-                if ui.button("select curve").clicked() {
-                    if let Some(curve_file) = rfd::FileDialog::new().pick_file() {
-                        let curve_data = fs::read_to_string(curve_file).unwrap();
-                        let curve = serde_json::from_str::<Spline<f64, f64>>(&curve_data).unwrap();
-                        apply_state.curve = Some(curve);
+                    if ui.button("Analyze Scan").clicked() {
+                        self.state = AppStates::Analyze(AnalyzeStates::WaitingForImage);
                     }
-                };
-                if ui.button("Output image").clicked() {
-                    if let Some(path) = rfd::FileDialog::new().save_file() {
-                        apply_state.output_file = Some(path);
+                    if ui.button("Apply Curve").clicked() {
+                        self.state = AppStates::Apply(ApplyStates::WaitingForImageAndCurve(
+                            WaitingForImageAndCurveState {
+                                curve: None,
+                                image: None,
+                            },
+                        ));
                     }
                 }
-
-                if let Some(preview) = &mut apply_state.preview {
-                    preview.ui(ui);
-                }
-
-                let can_apply = if apply_state.curve.is_some()
-                    && apply_state.image.is_some()
-                    && apply_state.output_file.is_some()
-                {
-                    true
-                } else {
-                    false
-                };
-
-                ui.add_enabled_ui(can_apply, |ui| {
-                    if ui.button("Apply").clicked() {
-                        println!("Apply!");
-                    }
-                });
-            }
-            AppStates::Analyze(analyze_state) => match analyze_state {
-                AnalyzeStates::WaitingForImage => {
-                    if ui.button("select scan").clicked() {
-                        if let Some(file) = rfd::FileDialog::new().pick_file() {
-                            let path = PathBuf::from(file.display().to_string());
-                            let image = image::open(&path).unwrap();
-
-                            let preview = TextureBufferedImage::new(
-                                path.clone().into_os_string().into_string().unwrap(),
-                                &image,
-                            );
-
-                            self.image_state = Some(ImageState {
-                                path: path.clone(),
-                                image,
-                                preview,
-                            });
-
-                            self.state = AppStates::Analyze(AnalyzeStates::ImageLoaded);
-                        }
-                    }
-                }
-                AnalyzeStates::ImageLoaded => {
-                    if let Some(image_state) = &mut self.image_state {
-                        if ui.button("analyze").clicked() {
-                            let analyze_results =
-                                analyze::analyze(&image_state.image, self.debug).unwrap();
-
-                            self.state =
-                                AppStates::Analyze(AnalyzeStates::Analyzed(analyze_results));
-                        }
-
+                AppStates::Generate => {
+                    // Consider writing these to a label in the image
+                    // Maybe store a list of procesess to choose from
+                    // and add new ones?
+                    ui.vertical(|ui| {
                         ui.horizontal(|ui| {
-                            ui.label("Picked file:");
-                            ui.monospace(image_state.path.to_string_lossy());
+                            let process_label = ui.label("Process: ");
+                            ui.text_edit_singleline(&mut self.process)
+                                .labelled_by(process_label.id);
                         });
-                        image_state.preview.ui(ui)
-                    }
-                }
-                AnalyzeStates::Analyzed(analyze_results) => {
-                    if ui.button("save curve").clicked() {
+                        ui.horizontal(|ui| {
+                            let notes_label = ui.label("Notes: ");
+                            ui.text_edit_singleline(&mut self.notes)
+                                .labelled_by(notes_label.id);
+                        });
+                    });
+
+                    if ui.button("Generate").clicked() {
                         if let Some(path) = rfd::FileDialog::new().save_file() {
-                            let curve_file = fs::File::create(path).unwrap();
-                            serde_json::to_writer(&curve_file, &analyze_results.curve).unwrap();
+                            let image = generate::generate(self.debug);
+                            image.save(path).unwrap();
                         }
                     }
                 }
-            },
+                AppStates::Apply(apply_state) => match apply_state {
+                    ApplyStates::WaitingForImageAndCurve(image_and_curve) => {
+                        if ui.button("Select Image").clicked() {
+                            if let Some(image_file) = rfd::FileDialog::new().pick_file() {
+                                let image = image::open(&image_file).unwrap();
+                                let preview = TextureBufferedImage::new(
+                                    image_file.clone().into_os_string().into_string().unwrap(),
+                                    &image,
+                                );
+                                image_and_curve.image = Some(image);
+                                self.preview = Some(preview)
+                            }
+                        };
+                        if ui.button("select curve").clicked() {
+                            if let Some(curve_file) = rfd::FileDialog::new().pick_file() {
+                                let curve_data = fs::read_to_string(curve_file).unwrap();
+                                let curve =
+                                    serde_json::from_str::<Spline<f64, f64>>(&curve_data).unwrap();
+                                image_and_curve.curve = Some(curve);
+                            }
+                        };
+
+                        if let Some(preview) = &mut self.preview {
+                            preview.ui(ui);
+                        }
+
+                        let can_apply =
+                            image_and_curve.curve.is_some() && image_and_curve.image.is_some();
+
+                        ui.add_enabled_ui(can_apply, |ui| {
+                            if ui.button("Apply").clicked() {
+                                self.state =
+                                    AppStates::Apply(ApplyStates::Applied(AppliedState {}));
+                            }
+                        });
+                    }
+                    // Maybe don't use a new state here but just shift the waiting state.
+                    ApplyStates::Applied(_applied_state) => {
+                        if ui.button("Save").clicked() {
+                            if let Some(_path) = rfd::FileDialog::new().save_file() {
+                                //save resulting image
+                            }
+                        }
+                    }
+                },
+                AppStates::Analyze(analyze_state) => match analyze_state {
+                    AnalyzeStates::WaitingForImage => {
+                        if ui.button("select scan").clicked() {
+                            if let Some(file) = rfd::FileDialog::new().pick_file() {
+                                let path = PathBuf::from(file.display().to_string());
+                                let image = image::open(&path).unwrap();
+
+                                let preview = TextureBufferedImage::new(
+                                    path.clone().into_os_string().into_string().unwrap(),
+                                    &image,
+                                );
+
+                                self.image_state = Some(ImageState {
+                                    path: path.clone(),
+                                    image,
+                                });
+                                self.preview = Some(preview);
+
+                                self.state = AppStates::Analyze(AnalyzeStates::ImageLoaded);
+                            }
+                        }
+                    }
+                    AnalyzeStates::ImageLoaded => {
+                        if let Some(image_state) = &mut self.image_state {
+                            if ui.button("analyze").clicked() {
+                                let analyze_results =
+                                    analyze::analyze(&image_state.image, self.debug).unwrap();
+
+                                self.state =
+                                    AppStates::Analyze(AnalyzeStates::Analyzed(analyze_results));
+                            }
+
+                            ui.horizontal(|ui| {
+                                ui.label("Picked file:");
+                                ui.monospace(image_state.path.to_string_lossy());
+                            });
+                            if let Some(preview) = &mut self.preview {
+                                preview.ui(ui)
+                            }
+                        }
+                    }
+                    AnalyzeStates::Analyzed(analyze_results) => {
+                        if ui.button("save curve").clicked() {
+                            if let Some(path) = rfd::FileDialog::new().save_file() {
+                                let curve_file = fs::File::create(path).unwrap();
+                                serde_json::to_writer(&curve_file, &analyze_results.curve).unwrap();
+                            }
+                        }
+                    }
+                },
+            }
         });
     }
 }

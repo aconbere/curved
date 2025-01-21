aaa3dad4c27752e7d93029ca170693ef8bd011a4
Anders Conbere <anders@conbere.org>
2025-01-20 21:28:46 -0800

    getting closer

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -14,47 +14,57 @@ mod texture_buffered_image;
 
 use texture_buffered_image::TextureBufferedImage;
 
-struct ImageState {
+struct PreviewedImage {
     path: PathBuf,
     image: DynamicImage,
-}
-
-enum AnalyzeStates {
-    WaitingForImage,
-    ImageLoaded,
-    Analyzed(analyze::AnalyzeResults),
-}
-
-struct ApplyState {
-    curve: Option<Spline<f64, f64>>,
-    image: Option<DynamicImage>,
-    curved_image: Option<DynamicImage>,
-}
-
-struct GenerateState {
-    process: String,
-    notes: String,
-    image: Option<DynamicImage>,
+    preview: TextureBufferedImage,
 }
 
 #[derive(Default)]
-enum AppStates {
+struct ApplyPageState {
+    curve: Option<Spline<f64, f64>>,
+    image: Option<PreviewedImage>,
+    curved_image: Option<PreviewedImage>,
+}
+
+#[derive(Default)]
+struct GeneratePageState {
+    process: String,
+    notes: String,
+    image: Option<PreviewedImage>,
+}
+
+#[derive(Default, PartialEq)]
+enum AnalyzePreviewTab {
+    #[default]
+    Scan,
+    Results,
+}
+
+#[derive(Default)]
+struct AnalyzePageState {
+    scan: Option<PreviewedImage>,
+    analysis: Option<analyze::AnalyzeResults>,
+    preview_tab: AnalyzePreviewTab,
+}
+
+#[derive(Default, PartialEq)]
+enum Page {
     #[default]
     Home,
-    Analyze(AnalyzeStates),
-    Generate(GenerateState),
-    Apply(ApplyState),
+    Analyze,
+    Generate,
+    Apply,
 }
 
 #[derive(Default)]
 struct CurvedApp {
-    state: AppStates,
     debug: bool,
-    image_state: Option<ImageState>,
 
-    generate_texture: Option<TextureBufferedImage>,
-    apply_texture: Option<TextureBufferedImage>,
-    analyze_texture: Option<TextureBufferedImage>,
+    page: Page,
+    generate_page_state: GeneratePageState,
+    analyze_page_state: AnalyzePageState,
+    apply_page_state: ApplyPageState,
 }
 
 impl CurvedApp {
@@ -82,7 +92,7 @@ impl CurvedApp {
 
 pub fn start(debug: bool) {
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([1200.0, 700.0]),
+        viewport: egui::ViewportBuilder::default().with_inner_size([1200.0, 900.0]),
         ..Default::default()
     };
 
@@ -97,72 +107,35 @@ pub fn start(debug: bool) {
     .unwrap();
 }
 
-fn app_state_to_string(state: &AppStates) -> String {
-    match state {
-        AppStates::Home => "generate-or-analyze".to_string(),
-        AppStates::Apply(_) => "apply".to_string(),
-        AppStates::Generate(_) => "generate".to_string(),
-        AppStates::Analyze(_) => "analyze".to_string(),
-    }
-}
-
-fn preview_area(ui: &mut egui::Ui, texture: &mut Option<TextureBufferedImage>) {
+fn preview_area(ui: &mut egui::Ui, texture: &mut TextureBufferedImage) {
     egui::Frame::none().fill(egui::Color32::RED).show(ui, |ui| {
         ui.set_min_width(500.0);
         ui.set_min_height(500.0);
         ui.vertical(|ui| {
-            if let Some(preview) = texture {
-                preview.ui(ui);
-            }
+            texture.ui(ui);
         });
     });
 }
 
 fn tab_bar(ui: &mut egui::Ui, app: &mut CurvedApp) {
-    let app_state_tab = app_state_to_string(&app.state);
-
     let buttons = vec![
-        (
-            "generate-or-analyze",
-            egui::Button::new("home"),
-            AppStates::Home,
-        ),
-        (
-            "generate",
-            egui::Button::new("generate"),
-            AppStates::Generate(GenerateState {
-                process: "".to_string(),
-                notes: "".to_string(),
-                image: None,
-            }),
-        ),
-        (
-            "analyze",
-            egui::Button::new("analyze"),
-            AppStates::Analyze(AnalyzeStates::WaitingForImage),
-        ),
-        (
-            "apply",
-            egui::Button::new("apply"),
-            AppStates::Apply(ApplyState {
-                curve: None,
-                curved_image: None,
-                image: None,
-            }),
-        ),
+        (Page::Home, egui::Button::new("home")),
+        (Page::Generate, egui::Button::new("generate")),
+        (Page::Analyze, egui::Button::new("analyze")),
+        (Page::Apply, egui::Button::new("apply")),
     ];
 
     ui.horizontal(|ui| {
-        for (name, button, new_state) in buttons {
-            let e = name != app_state_tab;
-            if ui.add_enabled(e, button).clicked() {
-                app.state = new_state;
+        for (name, button) in buttons {
+            let enabled = name != app.page;
+            if ui.add_enabled(enabled, button).clicked() {
+                app.page = name;
             };
         }
     });
 }
 
-fn home_page(ui: &mut egui::Ui, app: &mut CurvedApp) {
+fn home_page(ui: &mut egui::Ui) {
     ui.heading("Curved");
     ui.separator();
     ui.add_space(12.0);
@@ -172,142 +145,153 @@ fn home_page(ui: &mut egui::Ui, app: &mut CurvedApp) {
          results and use the Analyze function to produce a tone adjustment curve. Finally you can \
          apply the curve to an image with the Apply tool.",
     );
-    ui.add_space(12.0);
-    ui.heading("Generate Step Wedge");
-    ui.separator();
-    ui.label("Use this tool to build a fresh step wedge for your testing.");
-    if ui.button("Generate").clicked() {
-        app.state = AppStates::Generate(GenerateState {
-            process: "".to_string(),
-            notes: "".to_string(),
-            image: None,
-        });
-    }
-    ui.add_space(12.0);
-
-    ui.heading("Analyze Scan");
-    ui.separator();
-    ui.label("Use this tool build a tone adjustment curve from the scan of your print.");
-    if ui.button("Analyze").clicked() {
-        app.state = AppStates::Analyze(AnalyzeStates::WaitingForImage);
-    }
-    ui.add_space(12.0);
-
-    ui.heading("Apply Curve");
-    ui.separator();
-    ui.label("Use this tool apply a curve to an image you want to print.");
-    if ui.button("Apply").clicked() {
-        app.state = AppStates::Apply(ApplyState {
-            curve: None,
-            curved_image: None,
-            image: None,
-        });
-    }
 }
 
-fn generate_page(ui: &mut egui::Ui, app: &mut CurvedApp) {
-    if let AppStates::Generate(ref mut generate_state) = app.state {
-        let mut process = generate_state.process.clone();
-        let mut notes = generate_state.notes.clone();
+fn generate_page(ui: &mut egui::Ui, state: &mut GeneratePageState, debug: bool) {
+    let mut process = state.process.clone();
+    let mut notes = state.notes.clone();
 
-        ui.horizontal(|ui| {
-            ui.vertical(|ui| {
-                ui.set_max_width(300.0);
-                ui.label(
-                    "Generates a step wedge for printing as a transparency. Output will be a \
-                     16bit greyscale image. It is generated in its \"inverted\" form and does not \
-                     need to be further inverted before printing. A 300 dpi print should be about \
-                     5\"x5.25\".",
-                );
+    ui.horizontal(|ui| {
+        ui.vertical(|ui| {
+            ui.set_max_width(300.0);
+            ui.label(
+                "Generates a step wedge for printing as a transparency. Output will be a 16bit \
+                 greyscale image. It is generated in its \"inverted\" form and does not need to \
+                 be further inverted before printing. A 300 dpi print should be about 5\"x5.25\".",
+            );
 
-                let process_label = ui.label("Process: ");
-                ui.text_edit_singleline(&mut process)
-                    .labelled_by(process_label.id);
+            let process_label = ui.label("Process: ");
+            ui.text_edit_singleline(&mut process)
+                .labelled_by(process_label.id);
 
-                let notes_label = ui.label("Notes: ");
-                ui.text_edit_singleline(&mut notes)
-                    .labelled_by(notes_label.id);
+            let notes_label = ui.label("Notes: ");
+            ui.text_edit_singleline(&mut notes)
+                .labelled_by(notes_label.id);
 
-                if ui.button("Generate").clicked() {
-                    let image = generate::generate(app.debug);
-                    let preview = TextureBufferedImage::new(
-                        format!(
-                            "generated_step_wedge_{}_{}",
-                            generate_state.process, generate_state.notes
-                        ),
-                        &image,
-                    );
-                    generate_state.image = Some(image);
-                    app.generate_texture = Some(preview);
-                }
-                let has_image = generate_state.image.is_some();
-                if ui
-                    .add_enabled(has_image, egui::Button::new("Save"))
-                    .clicked()
-                {
-                    if let Some(path) = rfd::FileDialog::new().save_file() {
-                        if let Some(image) = &generate_state.image {
-                            image.save(path).unwrap();
-                        }
-                    }
-                }
-            });
-
-            preview_area(ui, &mut app.generate_texture);
-
-            generate_state.process = process.to_string();
-            generate_state.notes = notes.to_string();
-        });
-    }
-}
-
-fn apply_page(ui: &mut egui::Ui, app: &mut CurvedApp) {
-    if let AppStates::Apply(ref mut apply_state) = app.state {
-        if ui.button("Select Image").clicked() {
-            if let Some(image_file) = rfd::FileDialog::new().pick_file() {
-                let image = image::open(&image_file).unwrap();
+            if ui.button("Generate").clicked() {
+                let image = generate::generate(debug);
                 let preview = TextureBufferedImage::new(
-                    image_file.clone().into_os_string().into_string().unwrap(),
+                    format!("generated_step_wedge_{}_{}", state.process, state.notes),
                     &image,
                 );
-                apply_state.image = Some(image);
-                app.apply_texture = Some(preview);
+                state.image = Some(PreviewedImage {
+                    path: PathBuf::new(),
+                    image,
+                    preview,
+                });
             }
-        };
-
-        if ui.button("select curve").clicked() {
-            if let Some(curve_file) = rfd::FileDialog::new().pick_file() {
-                let curve_data = fs::read_to_string(curve_file).unwrap();
-                let curve = serde_json::from_str::<Spline<f64, f64>>(&curve_data).unwrap();
-                apply_state.curve = Some(curve);
+            if let Some(image) = &state.image {
+                if ui.button("Save").clicked() {
+                    if let Some(path) = rfd::FileDialog::new().save_file() {
+                        image.image.save(path).unwrap();
+                    }
+                };
+            } else {
+                ui.add_enabled(false, egui::Button::new("Save"));
             }
-        };
+        });
 
-        if let Some(preview) = &mut app.apply_texture {
-            preview.ui(ui);
+        if let Some(image) = &mut state.image {
+            preview_area(ui, &mut image.preview)
         }
 
-        if let (Some(image), Some(curve)) = (&apply_state.image, &apply_state.curve) {
-            if ui.button("Apply").clicked() {
-                let curved_image = apply::apply(&image, &curve);
+        state.process = process.to_string();
+        state.notes = notes.to_string();
+    });
+}
 
-                let preview =
-                    TextureBufferedImage::new("curved_image_preview".to_string(), &curved_image);
-                app.apply_texture = Some(preview);
-            }
+fn apply_page(ui: &mut egui::Ui, state: &mut ApplyPageState) {
+    if ui.button("Select Image").clicked() {
+        if let Some(path) = rfd::FileDialog::new().pick_file() {
+            let image = image::open(&path).unwrap();
+            let preview = TextureBufferedImage::new(
+                path.clone().into_os_string().into_string().unwrap(),
+                &image,
+            );
+            state.image = Some(PreviewedImage {
+                path,
+                image,
+                preview,
+            });
         }
+    };
 
-        if let Some(curved_image) = &apply_state.curved_image {
-            if ui.button("Save").clicked() {
-                if let Some(path) = rfd::FileDialog::new().save_file() {
-                    curved_image.save(path).unwrap();
-                }
+    if ui.button("select curve").clicked() {
+        if let Some(curve_file) = rfd::FileDialog::new().pick_file() {
+            let curve_data = fs::read_to_string(curve_file).unwrap();
+            let curve = serde_json::from_str::<Spline<f64, f64>>(&curve_data).unwrap();
+            state.curve = Some(curve);
+        }
+    };
+
+    if let Some(image) = &mut state.image {
+        image.preview.ui(ui);
+    }
+
+    if let (Some(image), Some(curve)) = (&state.image, &state.curve) {
+        if ui.button("Apply").clicked() {
+            let curved_image = apply::apply(&image.image, &curve);
+
+            let preview =
+                TextureBufferedImage::new("curved_image_preview".to_string(), &curved_image);
+            state.curved_image = Some(PreviewedImage {
+                path: image.path.clone(),
+                image: curved_image,
+                preview,
+            });
+        }
+    }
+
+    if let Some(curved_image) = &state.curved_image {
+        if ui.button("Save").clicked() {
+            if let Some(path) = rfd::FileDialog::new().save_file() {
+                curved_image.image.save(path).unwrap();
             }
         }
     }
 }
 
-fn analyze_page(ui: &mut egui::Ui, app: &mut CurvedApp) {
+fn analyze_results(ui: &mut egui::Ui, state: &mut AnalyzePageState) {
+    let buttons = vec![
+        (AnalyzePreviewTab::Scan, egui::Button::new("Scan")),
+        (AnalyzePreviewTab::Results, egui::Button::new("Results")),
+    ];
+
+    egui::Frame::none()
+        .fill(egui::Color32::BLUE)
+        .show(ui, |ui| {
+            ui.set_min_width(700.0);
+            ui.set_min_height(700.0);
+            ui.vertical(|ui| {
+                ui.horizontal(|ui| {
+                    for (name, button) in buttons {
+                        let enabled = name != state.preview_tab;
+                        if ui.add_enabled(enabled, button).clicked() {
+                            state.preview_tab = name;
+                        };
+                    }
+                });
+            });
+            if let Some(scan) = &mut state.scan {
+                preview_area(ui, &mut scan.preview);
+
+                let path: String = scan.path.to_string_lossy().to_string();
+                ui.monospace(path);
+            }
+            if let Some(analysis) = &state.analysis {
+                if ui.button("save").clicked() {
+                    if let Some(path) = rfd::FileDialog::new().save_file() {
+                        let curve_file = fs::File::create(path).unwrap();
+                        serde_json::to_writer(&curve_file, &analysis.curve).unwrap();
+                    }
+                };
+            } else {
+                let _ = ui.button("save");
+            }
+        });
+}
+
+fn analyze_page(ui: &mut egui::Ui, state: &mut AnalyzePageState, debug: bool) {
     ui.label(
         "This tool will evaluate a scan of the generated step wedge to generate a new curve file.",
     );
@@ -328,21 +312,18 @@ fn analyze_page(ui: &mut egui::Ui, app: &mut CurvedApp) {
                         &image,
                     );
 
-                    app.image_state = Some(ImageState {
+                    state.scan = Some(PreviewedImage {
                         path: path.clone(),
                         image,
+                        preview,
                     });
-                    app.analyze_texture = Some(preview);
-
-                    app.state = AppStates::Analyze(AnalyzeStates::ImageLoaded);
                 }
             }
 
-            if let Some(image_state) = &app.image_state {
+            if let Some(scan) = &state.scan {
                 if ui.add_enabled(true, egui::Button::new("analyze")).clicked() {
-                    let analyze_results = analyze::analyze(&image_state.image, app.debug).unwrap();
-
-                    app.state = AppStates::Analyze(AnalyzeStates::Analyzed(analyze_results));
+                    let analyze_results = analyze::analyze(&scan.image, debug).unwrap();
+                    state.analysis = Some(analyze_results);
                 }
             } else {
                 ui.add_enabled(false, egui::Button::new("analyze"));
@@ -350,20 +331,7 @@ fn analyze_page(ui: &mut egui::Ui, app: &mut CurvedApp) {
         });
 
         ui.vertical(|ui| {
-            preview_area(ui, &mut app.analyze_texture);
-            if let Some(image_state) = &app.image_state {
-                let path: String = image_state.path.to_string_lossy().to_string();
-                ui.monospace(path);
-            }
-
-            if let AppStates::Analyze(AnalyzeStates::Analyzed(analyze_results)) = &app.state {
-                if ui.button("save curve").clicked() {
-                    if let Some(path) = rfd::FileDialog::new().save_file() {
-                        let curve_file = fs::File::create(path).unwrap();
-                        serde_json::to_writer(&curve_file, &analyze_results.curve).unwrap();
-                    }
-                }
-            }
+            analyze_results(ui, state);
         });
     });
 }
@@ -374,18 +342,18 @@ impl eframe::App for CurvedApp {
             tab_bar(ui, self);
             ui.separator();
             ui.add_space(12.0);
-            match &mut self.state {
-                AppStates::Home => {
-                    home_page(ui, self);
+            match &mut self.page {
+                Page::Home => {
+                    home_page(ui);
                 }
-                AppStates::Generate(_) => {
-                    generate_page(ui, self);
+                Page::Generate => {
+                    generate_page(ui, &mut self.generate_page_state, self.debug);
                 }
-                AppStates::Apply(_) => {
-                    apply_page(ui, self);
+                Page::Apply => {
+                    apply_page(ui, &mut self.apply_page_state);
                 }
-                AppStates::Analyze(_) => {
-                    analyze_page(ui, self);
+                Page::Analyze => {
+                    analyze_page(ui, &mut self.analyze_page_state, self.debug);
                 }
             }
         });

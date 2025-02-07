4b20caadf770bc63cbf5f69d55ccd27fe8158635
Anders Conbere <anders@conbere.org>
2025-02-06 16:28:34 -0800

    fix some bugs with drawing the analysis rectangle

--- src/analyze.rs
+++ src/analyze.rs
@@ -1,6 +1,6 @@
 use anyhow::{anyhow, Result};
-use image::{DynamicImage, GenericImageView, ImageBuffer, Luma, SubImage};
-use imageproc::drawing::draw_filled_rect_mut;
+use image::{DynamicImage, GenericImageView, ImageBuffer, Luma, Rgb, SubImage};
+use imageproc::drawing::{draw_filled_rect_mut, draw_hollow_rect_mut};
 use imageproc::edges;
 use imageproc::hough;
 use imageproc::map::map_pixels;
@@ -37,7 +37,9 @@ pub fn analyze(image: &DynamicImage, debug: bool) -> anyhow::Result<AnalyzeResul
     let image_8 = image.to_luma8();
 
     let grid_analysis = analyze_grid(&image_8, debug)?;
-    let samples = collect_samples(&image_16, &step_description, &grid_analysis);
+    let sampled_areas = sampled_areas(&step_description, &grid_analysis);
+    let samples = collect_samples(&image_16, &sampled_areas);
+
     if debug {
         println!("Found: {} samples", samples.values.len());
         println!("sample min: {}", samples.min);
@@ -45,65 +47,26 @@ pub fn analyze(image: &DynamicImage, debug: bool) -> anyhow::Result<AnalyzeResul
         println!("dynamic range: {}", samples.max - samples.min);
     }
 
-    let (normalized_image, normalized_samples) =
-        normalize_image(&step_description, &image_16, &samples);
+    let NormalizedResults {
+        image: normalized_image,
+        samples: normalized_samples,
+    } = normalize_image(&step_description, &image_16, &samples);
 
-    /* Use our own observed values to find where we should place
-     * our points to curve with
-     *
-     * As an example let's assume we have a table like below:
-     *
-     * ---
-     * i    exp     norm
-     * ---
-     * 1    0       0
-     * 2    655     648
-     * ...
-     * 15   6550    5000
-     * ...
-     * 20   9825    6576
-     * ...
-     * 101  65500   64800
-     * ---
-     *
-     * Using this we can figure out how to push our densities around to get a linear relationship.
-     *
-     * like take the 15th step. In a linear relationshop the input density of 6550 would be
-     * perfectly reflected in the observed density 6550 -> 6550. But in our real world test it
-     * didn't.
-     *
-     * So instead we want to search through our observed densities and figure out what input
-     * density did create an ouput of 6550. Looking we can see step 20 was close.
-     *
-     * So when mapping our values when we render a new 15 we want the density to be 9825 so that it
-     * achieve an output density close to 6550.
-     *
-     * We'll find our value by finding the largest input density that is still less than our
-     * target, and the least input density that is still greater than our density. We'll then use
-     * the midpoint.
-     */
+    let curve_points = linearize_inputs(&input_values, &normalized_samples)?;
+    if debug {
+        println!("curve_points\n{:?}", curve_points);
+    }
+    let curve = best_fit_spline(&curve_points);
+    let histogram = create_histogram(&normalized_image, &grid_analysis, &step_description);
 
-    // assume a linear relationship, so every value of expected on the x
-    // axis should be expected on the y axis. Our observed values will be
-    // different. The curve is the delta.
-    let input_values_with_samples: Vec<(u16, u16)> = input_values
-        .clone()
-        .into_iter()
-        .zip(normalized_samples)
-        .collect();
-
-    let curve_points: anyhow::Result<Vec<(u16, u16)>> = input_values
-        .clone()
-        .into_iter()
-        .map(|e| find_closest_matching_input_density(&input_values_with_samples, e).map(|c| (e, c)))
-        .collect();
-
-    let curve = best_fit_spline(&curve_points?);
-    let histogram = create_histogram(&normalized_image);
+    println!("drawing analysis");
+    let normalized_image_with_rects =
+        draw_sampled_areas(&DynamicImage::ImageLuma16(normalized_image), &sampled_areas)?;
+    println!("done drawing analysis");
 
     Ok(AnalyzeResults {
         lines_image: grid_analysis.lines_image,
-        normalized_image: DynamicImage::ImageLuma16(normalized_image),
+        normalized_image: DynamicImage::ImageRgb8(normalized_image_with_rects),
         histogram,
         curve,
     })
@@ -166,16 +129,28 @@ fn sampled_mean(image: SubImage<&ImageBuffer<Luma<u16>, Vec<u16>>>) -> u16 {
     return (total / count) as u16;
 }
 
+fn draw_sampled_areas(
+    image: &DynamicImage,
+    rects: &Vec<Rect>,
+) -> Result<ImageBuffer<Rgb<u8>, Vec<u8>>> {
+    let mut image_rgb = image.to_rgb8();
+    let green = image::Rgb::<u8>([0, 255, 0]);
+    for r in rects {
+        draw_hollow_rect_mut(&mut image_rgb, *r, green)
+    }
+    Ok(image_rgb)
+}
+
 pub fn draw_curve(
     image: &mut ImageBuffer<image::Rgb<u8>, Vec<u8>>,
     curve: &Spline<f64, f64>,
-) -> anyhow::Result<()> {
+) -> Result<()> {
     for i in (0..u16::MAX).step_by(64) {
         let green = image::Rgb::<u8>([0, 255, 0]);
         let sample = curve
             .clamped_sample(i as f64)
             .ok_or(anyhow!("failed to sample spline"))?;
-        let y = 1024 - (sample / 64.) as u32;
+        let y = 1023 - (sample / 64.) as u32;
         let x = (i / 64) as u32;
         image.put_pixel(x, y, green);
     }
@@ -236,19 +211,14 @@ fn find_closest_matching_input_density(
     let mut lower_bound_density: Option<u16> = None;
     let mut upper_bound_density: Option<u16> = None;
 
-    // search forwardd, find the first output_density /greater/ than needle
+    // search forward, find the first output_density /greater/ than needle
     // our lower bound will then be the input density immediately prior
     for (i, (_, output_density)) in haystack.into_iter().enumerate() {
-        if *output_density >= needle {
+        if *output_density > needle {
             if i == 0 {
                 lower_bound_density = Some(0);
-                break;
-            }
-            // if we found an exact match don't look backwards
-            if *output_density == needle {
-                lower_bound_density = Some(haystack[i].0)
             } else {
-                lower_bound_density = Some(haystack[i - 1].0)
+                lower_bound_density = Some(haystack[i - 1].0);
             }
 
             break;
@@ -258,16 +228,11 @@ fn find_closest_matching_input_density(
     // search backwards, find the first output_density /lesser/ than needle
     // our upper bound will then be the input density immediately prior
     for (i, (_, output_density)) in haystack.into_iter().rev().enumerate() {
-        if *output_density <= needle {
+        if *output_density < needle {
             if i == 0 {
                 upper_bound_density = Some(u16::MAX);
-                break;
-            }
-
-            if *output_density == needle {
-                upper_bound_density = Some(haystack[haystack.len() - i].0)
             } else {
-                upper_bound_density = Some(haystack[haystack.len() - i - 1].0)
+                upper_bound_density = Some(haystack[haystack.len() - i].0);
             }
 
             break;
@@ -287,10 +252,23 @@ fn find_closest_matching_input_density(
 }
 
 // simple histogram of the image with 256 buckets
-fn create_histogram(image: &ImageBuffer<Luma<u16>, Vec<u16>>) -> Vec<u32> {
+fn create_histogram(
+    image: &ImageBuffer<Luma<u16>, Vec<u16>>,
+    grid_analysis: &GridAnalysis,
+    step_description: &StepDescription,
+) -> Vec<u32> {
+    let view = image
+        .view(
+            grid_analysis.origin_x + 5,
+            grid_analysis.origin_y + 5,
+            grid_analysis.square_size * step_description.columns,
+            grid_analysis.square_size * step_description.rows,
+        )
+        .to_image();
+
     let mut histogram: Vec<u32> = vec![0; 256];
 
-    for (_, _, p) in image.enumerate_pixels() {
+    for (_, _, p) in view.enumerate_pixels() {
         let bucket = (p[0] / 256) as usize;
         histogram[bucket] = histogram[bucket].saturating_add(1)
     }
@@ -305,6 +283,12 @@ struct GridAnalysis {
     lines_image: DynamicImage,
 }
 
+// Analyzes `image` looking for the grid of squares
+//
+// returns the discovered x,y cordinates of the top left corner of the grid, the observed square
+// size, as well as the lines image used for rendering the results
+//
+// Note: Consider making the lines image a function so we don't have to pre-compute?
 fn analyze_grid(image: &ImageBuffer<Luma<u8>, Vec<u8>>, debug: bool) -> Result<GridAnalysis> {
     // Canny is an edge detection algorithm, it's the input to the hough transform
     // we'll use later to do line detection
@@ -368,46 +352,30 @@ struct Samples {
     max: u16,
 }
 
-fn collect_samples(
-    image: &ImageBuffer<Luma<u16>, Vec<u16>>,
-    step_description: &StepDescription,
-    grid_analysis: &GridAnalysis,
-) -> Samples {
-    let mut values: Vec<u16> = vec![0; step_description.count as usize];
-    let mut n: usize = 0;
+fn collect_samples(image: &ImageBuffer<Luma<u16>, Vec<u16>>, rects: &Vec<Rect>) -> Samples {
+    let mut values: Vec<u16> = vec![0; rects.len()];
     let mut max: u16 = 0;
     let mut min: u16 = u16::MAX;
 
-    for row in 0..step_description.rows {
-        for col in 0..step_description.columns {
-            if n >= step_description.count as usize {
-                break;
-            }
-            let x = grid_analysis.origin_x + (col * grid_analysis.square_size);
-            let y = grid_analysis.origin_y + (row * grid_analysis.square_size);
+    for (i, r) in rects.iter().enumerate() {
+        let view = image.view(r.left() as u32, r.top() as u32, r.width(), r.height());
+        let sample = sampled_mean(view);
 
-            // this is a "window" of the square, stepped in 25-30 pixels on each side so as
-            // to avoid any malarky with the ednge of the square or the number on the top
-            // left corner
-            let view = image.view(
-                x + 25,
-                y + 25,
-                grid_analysis.square_size - 30,
-                grid_analysis.square_size - 30,
-            );
-            let sample = sampled_mean(view);
-
-            values[n] = sample;
-            if sample > max {
-                max = sample;
-            }
-            if sample < min {
-                min = sample;
-            }
-            n += 1;
+        values[i] = sample;
+        if sample > max {
+            max = sample;
+        }
+        if sample < min {
+            min = sample;
         }
     }
+
     Samples { values, max, min }
+}
+
+struct NormalizedResults {
+    image: ImageBuffer<Luma<u16>, Vec<u16>>,
+    samples: Vec<u16>,
 }
 
 // Now normalize the samples based on the maximum and minimum values
@@ -419,7 +387,7 @@ fn normalize_image(
     step_description: &StepDescription,
     image: &ImageBuffer<Luma<u16>, Vec<u16>>,
     samples: &Samples,
-) -> (ImageBuffer<Luma<u16>, Vec<u16>>, Vec<u16>) {
+) -> NormalizedResults {
     /* example
      *
      * Suppose we have:
@@ -451,7 +419,96 @@ fn normalize_image(
         Luma([(new_v as f32 * normalize_factor) as u16])
     });
 
-    (normalized_image, normalized_samples)
+    NormalizedResults {
+        image: normalized_image,
+        samples: normalized_samples,
+    }
+}
+
+/* Use our own observed values to find where we should place
+ * our points to curve with
+ *
+ * As an example let's assume we have a table like below:
+ *
+ * ---
+ * i    exp     norm
+ * ---
+ * 1    0       0
+ * 2    655     648
+ * ...
+ * 15   6550    5000
+ * ...
+ * 20   9825    6576
+ * ...
+ * 101  65500   64800
+ * ---
+ *
+ * Using this we can figure out how to push our densities around to get a linear relationship.
+ *
+ * like take the 15th step. In a linear relationshop the input density of 6550 would be
+ * perfectly reflected in the observed density 6550 -> 6550. But in our real world test it
+ * didn't.
+ *
+ * So instead we want to search through our observed densities and figure out what input
+ * density did create an ouput of 6550. Looking we can see step 20 was close.
+ *
+ * So when mapping our values when we render a new 15 we want the density to be 9825 so that it
+ * achieve an output density close to 6550.
+ *
+ * We'll find our value by finding the largest input density that is still less than our
+ * target, and the least input density that is still greater than our density. We'll then use
+ * the midpoint.
+ */
+fn linearize_inputs(
+    input_values: &Vec<u16>,
+    normalized_samples: &Vec<u16>,
+) -> Result<Vec<(u16, u16)>> {
+    // assume a linear relationship, so every value of expected on the x
+    // axis should be expected on the y axis. Our observed values will be
+    // different. The curve is the delta.
+    let input_values_with_samples: Vec<(u16, u16)> = input_values
+        .clone()
+        .into_iter()
+        .zip(normalized_samples.into_iter().copied())
+        .collect();
+
+    println!("input_values\n: {:?}\n", input_values);
+    println!("normalized_samples\n: {:?}\n", normalized_samples);
+
+    input_values
+        .clone()
+        .into_iter()
+        .map(|e| find_closest_matching_input_density(&input_values_with_samples, e).map(|c| (e, c)))
+        .collect()
+}
+
+fn sampled_areas(step_description: &StepDescription, grid_analysis: &GridAnalysis) -> Vec<Rect> {
+    let mut n: usize = 0;
+    let mut rects = Vec::new();
+
+    // 10% margin around the whole square
+    let margin = (grid_analysis.square_size as f32 * 0.25).floor() as u32;
+    let analyzed_size = grid_analysis.square_size - (2 * margin);
+
+    println!("margin: {}", margin);
+
+    for row in 0..step_description.rows {
+        for col in 0..step_description.columns {
+            if n >= step_description.count as usize {
+                break;
+            }
+            let x = grid_analysis.origin_x + (col * grid_analysis.square_size) + margin;
+            let y = grid_analysis.origin_y + (row * grid_analysis.square_size) + margin;
+
+            // this is a "window" of the square, stepped in 25-30 pixels on each side so as
+            // to avoid any malarky with the ednge of the square or the number on the top
+            // left corner
+            let rect = Rect::at(x as i32, y as i32).of_size(analyzed_size, analyzed_size);
+            rects.push(rect);
+            n += 1;
+        }
+    }
+    rects
 }
 
 #[cfg(test)]
@@ -481,18 +538,55 @@ mod tests {
 
     #[test]
     fn test_find_closest_matching_input_density() {
-        let haystack = vec![(1, 1), (2, 4), (3, 9), (4, 16), (5, 25)];
-        let mut result = find_closest_matching_input_density(&haystack, 2).unwrap();
-        assert_eq!(result, 1);
+        let haystack = vec![
+            (1, 0),
+            (2, 0),
+            (3, 0),
+            (4, 4),
+            (5, 9),
+            (6, 16),
+            (7, 25),
+            (8, 25),
+            (9, 25),
+            (10, 25),
+        ];
+        // l_bound = 3
+        // u_bound = None
+        // result = floor((3 + 4) / 2) => 3
+        let mut result = find_closest_matching_input_density(&haystack, 0).unwrap();
+        assert_eq!(result, 3);
 
+        // l_bound = 3
+        // u_bound = None
+        // result = floor((3 + 4) / 2) => 3
+        result = find_closest_matching_input_density(&haystack, 1).unwrap();
+        assert_eq!(result, 3);
+
+        // l_bound = None
+        // u_bound = 7
+        result = find_closest_matching_input_density(&haystack, 25).unwrap();
+        assert_eq!(result, 7);
+
+        // l_bound = 3
+        // u_bound = 4
+        // result = floor((3 + 4) / 2) => 3
+        result = find_closest_matching_input_density(&haystack, 2).unwrap();
+        assert_eq!(result, 3);
+
+        // l_bound = 4
+        // u_bound = 5
+        // result = floor((4 + 5) / 2) => 4
         result = find_closest_matching_input_density(&haystack, 5).unwrap();
-        assert_eq!(result, 2);
-
-        result = find_closest_matching_input_density(&haystack, 19).unwrap();
         assert_eq!(result, 4);
+
+        // l_bound = 6
+        // u_bound = 7
+        // result = floor((6 + 7) / 2) => 6
+        result = find_closest_matching_input_density(&haystack, 19).unwrap();
+        assert_eq!(result, 6);
 
         // check an exact match
         result = find_closest_matching_input_density(&haystack, 9).unwrap();
-        assert_eq!(result, 3);
+        assert_eq!(result, 5);
     }
 }
